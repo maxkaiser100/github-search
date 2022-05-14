cab722374958c74a9d83ec2a32fd3fefdc1168b0
maxkaiser100 <max@videopipeline.io>
2022-05-14 13:23:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Frontend Mentor - GitHub user search app solution
+
+This is a solution to the [GitHub user search app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+### Screenshot
+
+![](./assets/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Sass
+- vanilla JS
+
+
+
+## Author
+
+- Frontend Mentor - @maxKaiser
---
+++ app.js
@@ -0,0 +1,107 @@
+const userName = document.querySelector('.userName');
+const login = document.querySelector('.login');
+const profile = document.querySelector('.profile');
+const repoNumber = document.querySelector('.repoNumber');
+const followersNumber = document.querySelector('.followersNumber');
+const followingNumber = document.querySelector('.followingNumber');
+const city = document.querySelector('.cityText');
+const url = document.querySelector('.urlText');
+const twitter = document.querySelector('.twitterText');
+const company = document.querySelector('.companyText');
+const dateArea = document.querySelector('.rightProfile');
+const searchForm = document.querySelector('form');
+const iconImage = document.querySelector('.iconImage');
+const input = document.getElementById('.form-input');
+
+// api url
+const api_url =
+	"https://api.github.com/users/";
+
+// Defining async function
+async function getapi(url, searchPerson) {
+	
+	// Storing response
+	const response = await fetch(url + searchPerson);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+    if(data.message === 'Not Found') {
+        console.log('no good');
+        searchForm.reset();
+        document.getElementById('form-input').placeholder = "Not found";
+    } else {
+	show(data);
+    }
+}
+
+// Calling that async function
+
+
+searchForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    const searchPerson = searchForm.userSearch.value.trim();
+  
+    // const searchPerson = ;
+    // console.log(searchPerson);
+    // searchForm.reset();
+    getapi(api_url, searchPerson);
+})
+
+function show(data){
+    const date1 = new Date(data.created_at);
+    dateArea.innerHTML = `Joined ${date1.toDateString()}`;
+
+    userName.innerHTML = data.name;
+    login.innerHTML = `@${data.login}`;
+    
+    if(data.bio === null) {
+        profile.innerHTML = 'This profile has no bio'
+        profile.classList.remove('whiteText');
+    } else {
+        profile.innerHTML = data.bio;
+        profile.classList.add('whiteText');
+    }
+
+    iconImage.innerHTML =  `<img src="${data.avatar_url}">`;
+    repoNumber.innerHTML = data.public_repos;
+    followersNumber.innerHTML = data.followers;
+    followingNumber.innerHTML = data.following;
+    
+    if(data.location === null) {
+        city.innerHTML = 'Not available'
+        city.classList.remove('whiteText');
+    } else {
+        city.innerHTML = data.location;
+        city.classList.add('whiteText');
+        
+    }
+
+    
+    if(data.blog === null || data.blog === '') {
+        url.innerHTML = 'Not available'
+        url.classList.remove('whiteText');
+    } else {
+        url.innerHTML = `<a href="${data.blog}">${data.blog}</a>`;
+        url.classList.add('whiteText');
+        
+    }
+
+    if(data.twitter_username === null) {
+        twitter.innerHTML = 'Not available'
+    } else {
+        twitter.innerHTML = data.twitter_username;
+    }
+
+    if(data.company === null) {
+        company.innerHTML = 'Not available'
+        company.classList.remove('whiteText');
+    } else {
+        company.innerHTML = data.company;
+        company.classList.add('whiteText');
+    }
+    
+
+
+
+}
