cf257d77d0a2ad8b5d989bba03073d35a763ac07
Florian Jourde <jourdeflorian@gmail.com>
2022-05-15 02:00:15 +0200

    Adjusting CSS about responsiveness and conventions

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const userName = document.querySelector('.userName');
+const userName = document.querySelector('.user-name');
 const login = document.querySelector('.login');
 const profile = document.querySelector('.profile');
-const repoNumber = document.querySelector('.repoNumber');
-const followersNumber = document.querySelector('.followersNumber');
-const followingNumber = document.querySelector('.followingNumber');
-const city = document.querySelector('.cityText');
-const url = document.querySelector('.urlText');
-const twitter = document.querySelector('.twitterText');
-const company = document.querySelector('.companyText');
-const dateArea = document.querySelector('.rightProfile');
+const repoNumber = document.querySelector('.repo-number');
+const followersNumber = document.querySelector('.followers-number');
+const followingNumber = document.querySelector('.following-number');
+const city = document.querySelector('.city-text');
+const url = document.querySelector('.url-text');
+const twitter = document.querySelector('.twitter-text');
+const company = document.querySelector('.company-text');
+const dateArea = document.querySelector('.right-profile');
 const searchForm = document.querySelector('form');
-const iconImage = document.querySelector('.iconImage');
+const iconImage = document.querySelector('.icon-image');
 const input = document.getElementById('.form-input');
 
 // api url
 const api_url =
-	"https://api.github.com/users/";
+  "https://api.github.com/users/";
 
 // Defining async function
 async function getapi(url, searchPerson) {
-	
-	// Storing response
-	const response = await fetch(url + searchPerson);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
+
+    // Storing response
+    const response = await fetch(url + searchPerson);
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
     if(data.message === 'Not Found') {
         console.log('no good');
         searchForm.reset();
         document.getElementById('form-input').placeholder = "Not found";
     } else {
-	show(data);
+        show(data);
     }
 }
 
@@ -41,7 +41,7 @@ async function getapi(url, searchPerson) {
 searchForm.addEventListener('submit', e=> {
     e.preventDefault();
     const searchPerson = searchForm.userSearch.value.trim();
-  
+
     // const searchPerson = ;
     // console.log(searchPerson);
     // searchForm.reset();
@@ -54,37 +54,37 @@ function show(data){
 
     userName.innerHTML = data.name;
     login.innerHTML = `@${data.login}`;
-    
+
     if(data.bio === null) {
         profile.innerHTML = 'This profile has no bio'
-        profile.classList.remove('whiteText');
+        profile.classList.remove('white-text');
     } else {
         profile.innerHTML = data.bio;
-        profile.classList.add('whiteText');
+        profile.classList.add('white-text');
     }
 
     iconImage.innerHTML =  `<img src="${data.avatar_url}">`;
     repoNumber.innerHTML = data.public_repos;
     followersNumber.innerHTML = data.followers;
     followingNumber.innerHTML = data.following;
-    
+
     if(data.location === null) {
         city.innerHTML = 'Not available'
-        city.classList.remove('whiteText');
+        city.classList.remove('white-text');
     } else {
         city.innerHTML = data.location;
-        city.classList.add('whiteText');
-        
+        city.classList.add('white-text');
+
     }
 
-    
+
     if(data.blog === null || data.blog === '') {
         url.innerHTML = 'Not available'
-        url.classList.remove('whiteText');
+        url.classList.remove('white-text');
     } else {
         url.innerHTML = `<a href="${data.blog}">${data.blog}</a>`;
-        url.classList.add('whiteText');
-        
+        url.classList.add('white-text');
+
     }
 
     if(data.twitter_username === null) {
@@ -95,13 +95,9 @@ function show(data){
 
     if(data.company === null) {
         company.innerHTML = 'Not available'
-        company.classList.remove('whiteText');
+        company.classList.remove('white-text');
     } else {
         company.innerHTML = data.company;
-        company.classList.add('whiteText');
+        company.classList.add('white-text');
     }
-    
-
-
-
 }
